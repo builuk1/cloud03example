e5fbff9d9d78aa8573d1bc2f63523f20b497abf5
Green <name>
2022-05-02 20:49:43 +0300

    Starter

---
+++ example.py
@@ -0,0 +1,8 @@
+#Выбрать через консоль нужную директорию cd /d D:\
+#git clone git@github.com:builuk1/cloud03example.git
+#Win venv\Scripts\activate    OS/UNIX venv/bin/activate
+#venv python 3.10
+#python3 -m pip install --upgrade pip
+#pip install -r requirements.txt
+#settings > tools> python integrateg tools > testing > pytest
+#C:\Program Files (x86)\Google\Chrome\Application add driver
